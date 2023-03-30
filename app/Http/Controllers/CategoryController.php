7bfd79f74481f6ca581a397f36daff353e55a44f
PhaNith <nochphanith@gmail.com>
2023-03-30 18:54:54 +0700

    added store categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    //
+    public function index(){
+        return view('backend.category.index');
+    }
+    public function create(){
+        return view('backend.category.create');
+    }
+    
+    public function store(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required',
+        ]);
+        Category::create($request->all());
+        return redirect()->route('category')->with('success', 'Category created successfully.');
+    }
 }
--- resources/views/components/sidebar/content.blade.php
+++ resources/views/components/sidebar/content.blade.php
@@ -13,7 +13,15 @@
             <x-icons.dashboard class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
         </x-slot>
     </x-sidebar.link>
-
+    <x-sidebar.link
+        title="Categories"
+        href="{{ route('category') }}"
+        :isActive="request()->routeIs('category')"
+    >
+        <x-slot name="icon">
+            <x-icons.dashboard class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
+        </x-slot>
+    </x-sidebar.link>
     <x-sidebar.dropdown
         title="Buttons"
         :active="Str::startsWith(request()->route()->uri(), 'buttons')"
